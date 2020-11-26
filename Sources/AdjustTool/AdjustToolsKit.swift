ea0db55a9468af813fb963b39376b1c237bafd92
Lilia Lashin <lilia@tap.pm>
2020-11-26 10:07:10 +0200

    added log

--- Sources/AdjustTool/AdjustToolsKit.swift
+++ Sources/AdjustTool/AdjustToolsKit.swift
@@ -39,6 +39,7 @@ public class AdjustToolsKit {
     @available(iOS 10.0, *)
     public func clearContextCaches() {
         context.clearCaches()
+        print(">>>>>> CIContext cleared caches")
      }
     
     // MARK: - APPLY
